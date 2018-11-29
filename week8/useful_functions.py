33febae995044712609eda65706335d813fb2cc2
afspies <alexfspies@gmail.com>
2018-11-29 16:13:28 +0000

    more stuff

--- week8/useful_functions.py
+++ week8/useful_functions.py
@@ -1,19 +1,26 @@
+# This is only here because Jupyter sucks
 def login():
-	import spotipy 
-	import spotipy.util as util
-	token = util.prompt_for_user_token('Python Classes','user-library-read',
-			    client_id='76b2fa9353c84080a56db4ab3857503b',
-			    client_secret='5b99dc662e3f47b188ea08bdf706669a',
-			    redirect_uri='http://localhost/')
+    import spotipy 
+    import spotipy.util as util
+    scope = 'playlist-modify-public user-modify-playback-state'
+    token = util.prompt_for_user_token('Python Classes',scope,
+                client_id='76b2fa9353c84080a56db4ab3857503b',
+                client_secret='5b99dc662e3f47b188ea08bdf706669a',
+                redirect_uri='http://localhost/')
 
-	return spotipy.Spotify(auth=token)
+    return spotipy.Spotify(auth=token)
 
 def show_picture(url):
-	from PIL import Image
-	import requests
-	from io import BytesIO
-	response = requests.get(url)
-	img = Image.open(BytesIO(response.content))
-	img.show()
-	return
+    from PIL import Image
+    import requests
+    from io import BytesIO
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img.show()
+    return
 
+def add_song(username, tracks, spotify_obj, playlist_id='7mygqiVTPoGfxuRF5xequX'):
+    if isinstance(tracks, str):
+        tracks = [tracks]
+    spotify_obj.user_playlist_add_tracks(username, playlist_id, tracks)
+# ^ IGNORE THIS STUFF ^
